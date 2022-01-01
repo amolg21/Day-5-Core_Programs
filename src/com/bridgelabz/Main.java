9bb848447bb95e288379686a4cb286b037904e16
amol ghotale <amol9.ag@gmail.com>
2022-01-01 23:38:55 +0530

    MODIFIED: ADDED leap year

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,31 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double tails = 0;
-        double heads = 0;
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of coin flips (x)= ");
-        int x = sc.nextInt();
-        int flip = 1;
+        System.out.println("Enter the year (four digit only)= ");
+        int year = sc.nextInt();
 
-        for (flip=1; flip<=x; flip++) {
+        if (year % 4 == 0) {
+            System.out.println("This is a leap year");
 
-            double flip_check = Math.floor(Math.random() * 10) % 2;
-            System.out.println(flip_check);
-            if (flip_check < 0.5) {
-                System.out.println("It is Tails");
-                tails = tails + 1;
-            } else {
-                System.out.println("It is Heads");
-                heads = heads + 1;
-            }
-            System.out.println("Number of times tails= "+tails);
-            System.out.println("Number of times heads= "+heads);
+        } else {
+            System.out.println("Not a leap year");
         }
-            double c = (heads / x) * 100;
-            double d = (tails / x) * 100;
-
-        System.out.println("Percentage of Heads= " + c);
-        System.out.println("Percentage of Tails= " + d);
     }
 }
+
+
+
+
+
