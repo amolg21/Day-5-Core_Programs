6bdfd9c0010096426d3d308f55dff81213747648
amol ghotale <amol9.ag@gmail.com>
2022-01-01 19:19:28 +0530

    ADDED: Flip Coin

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,8 +1,35 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
-public class Core {
+public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        double tails = 0;
+        double heads = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of coin flips (x)= ");
+        int x = sc.nextInt();
+        int flip = 1;
+
+        for (flip=1; flip<=x; flip++) {
+
+            double flip_check = Math.floor(Math.random() * 10) % 2;
+            System.out.println(flip_check);
+            if (flip_check < 0.5) {
+                System.out.println("It is Tails");
+                tails = tails + 1;
+            } else {
+                System.out.println("It is Heads");
+                heads = heads + 1;
+            }
+            System.out.println("Number of times tails= "+tails);
+            System.out.println("Number of times heads= "+heads);
+        }
+            double c = (heads / x) * 100;
+            double d = (tails / x) * 100;
+
+        System.out.println("Percentage of Heads= " + c);
+        System.out.println("Percentage of Tails= " + d);
     }
 }
